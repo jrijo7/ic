59be2d14b978cdbd59b60e8ee3dde2c38e264a65
Carlos Tadeu Pagani Zanini <carlostpz@utexas.edu>
2024-03-08 19:37:43 -0300

    Update utils.R
    transform function now handles dataframe as input

--- codes/utils.R
+++ codes/utils.R
@@ -1,14 +1,27 @@
 transform <- function(data_mat
 ){
-  D = max(data_mat[, 2])
-  w = rep( list(NULL), D)
-  for (d in 1:D){
-    doc_d = data_mat[which(data_mat$doc == d), c(1,3)]
-    print(paste('d = ', d))
-    for (i in 1:nrow(doc_d) ){
-      w[[d]] = c(w[[d]], rep( doc_d$word[i] - 1, doc_d$freq[i]) )
-    } 
+  if (is.data.frame(data_mat)){
+    D = max(data_mat[, 2])
+    w = rep( list(NULL), D)
+    for (d in 1:D){
+      doc_d = data_mat[which(data_mat$doc == d), c(1,3)]
+      print(paste('d = ', d))
+      for (i in 1:nrow(doc_d) ){
+        w[[d]] = c(w[[d]], rep( doc_d$word[i] - 1, doc_d$freq[i]) )
+      } 
+    }
+  }else{
+    if(is.matrix(data_mat)){
+      D = max(data_mat[, 2])
+      w = rep( list(NULL), D)
+      for (d in 1:D){
+        doc_d = data_mat[which(data_mat[,"doc"] == d), c(1,3)]
+        print(paste('d = ', d))
+        for (i in 1:nrow(doc_d) ){
+          w[[d]] = c(w[[d]], rep( doc_d[i,"word"] - 1, doc_d[i, "freq"]) )
+        } 
+      }
+    }
   }
   return(w)
 }
-
